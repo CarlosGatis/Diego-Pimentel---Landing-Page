81ffab73ca58fc7cb566abb441d48f96af3b32ab
Carlos Gatis <112558520+CarlosGatis@users.noreply.github.com>
2026-06-02 13:05:17 -0300

    Delete js directory

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.menu-panel a').forEach(link => {
-  link.addEventListener('click', () => {
-    document.querySelector('.menu-dropdown').removeAttribute('open');
-  });
-});
